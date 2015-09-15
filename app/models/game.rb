6098c841b4eb85d8118b99a6449285c41e63b570
Colby M. White <colby.white@utexas.edu>
2015-09-14 23:53:21 -0500

    remove unneeded selfs; better hash syntax #rubocop

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,12 @@
 class Game < ActiveRecord::Base
-  belongs_to :home, :class_name => 'Team', :foreign_key => :home_id
-  belongs_to :away, :class_name => 'Team', :foreign_key => :away_id
+  belongs_to :home, class_name: 'Team', foreign_key: :home_id
+  belongs_to :away, class_name: 'Team', foreign_key: :away_id
 
   def self.by_team(team_id)
     where('home_id = :team_id OR away_id = :team_id', team_id: team_id)
   end
 
   def self.eager_load_teams
-    self.eager_load(:home).eager_load(:away)
+    eager_load(:home).eager_load(:away)
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,12 +1,12 @@
 class Team < ActiveRecord::Base
-  has_many :home_games, :class_name => 'Game', :foreign_key=>:home_id
-  has_many :away_games, :class_name => 'Game', :foreign_key=>:away_id
+  has_many :home_games, class_name: 'Game', foreign_key: :home_id
+  has_many :away_games, class_name: 'Game', foreign_key: :away_id
 
   def games
-    Game.by_team self.id
+    Game.by_team id
   end
 
   def logo
-    ActionController::Base.helpers.asset_path("logos/#{self.abbr.downcase}.png")
+    ActionController::Base.helpers.asset_path("logos/#{abbr.downcase}.png")
   end
 end
