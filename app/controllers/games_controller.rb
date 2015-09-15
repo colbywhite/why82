dcd4e2cdb1dcc41b70420083c530cfe028a78fa9
Colby M. White <colby.white@utexas.edu>
2015-09-14 23:39:52 -0500

    refactoring to make index logic simplier #rubocop
    Biggest thing was creating `fetch_games` and
    `parse_teams_from_params` methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,39 +1,37 @@
 class GamesController < ApplicationController
   include GamesHelper
 
-  def get_set(arr)
-    (arr || []).to_set
-  end
-
   def index
-    away_teams = get_set params[:away_teams]
-    home_teams = get_set params[:home_teams]
-    teams = get_set params[:teams]
-    do_or = ActiveRecord::Type::Boolean.new.type_cast_from_user params[:or]
-    away_teams += teams
-    home_teams += teams
-    away_teams = away_teams.collect { |t| t.to_i }
-    home_teams = home_teams.collect { |t| t.to_i }
-    puts "Away: #{away_teams.inspect}"
-    puts "Home: #{home_teams.inspect}"
-    @all = Game.eager_load_teams.order(:time)
-    if (away_teams.count + home_teams.count) >0
-      if do_or
-        @all = @all.where{away_id >> away_teams.to_a | home_id >> home_teams.to_a}
-      else
-        if away_teams.empty?
-          @all = @all.where{home_id >> home_teams.to_a}
-        elsif home_teams.empty?
-          @all = @all.where{away_id >> away_teams.to_a}
-        else
-          @all = @all.where{away_id >> away_teams.to_a & home_id >> home_teams.to_a}
-        end
-      end
-    end
+    home_teams, away_teams = parse_teams_from_params
+    @all = fetch_games home_teams, away_teams
     respond_to do |format|
-      puts format
       format.html # index.html.erb
       format.json { render json: @all.as_json(game_json_options) }
     end
+  end
+
+  def parse_teams_from_params
+    teams = to_i_set params[:teams]
+    away_teams = to_i_set(params[:away_teams]) + teams
+    home_teams = to_i_set(params[:home_teams]) + teams
+    [home_teams, away_teams]
+  end
+
+  # converts array to a set with integers
+  def to_i_set(arr)
+    string_set = (arr || []).to_set
+    to_i_collection string_set
+  end
+
+  # converts every ele in collection to integer
+  def to_i_collection(collection)
+    collection.collect(&:to_i)
+  end
+
+  def fetch_games(home_teams, away_teams)
+    games = Game.eager_load_teams.order(:time)
+    games = games.where { home_id >> home_teams } if home_teams.count > 0
+    games = games.where { away_id >> away_teams } if away_teams.count > 0
+    games
   end
 end
