d16d2cab998f67694a84cfd5e78447969ea60478
Colby M. White <colby.white@utexas.edu>
2015-09-15 01:05:23 -0500

    defining module ActiveSupport, not class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport
+module ActiveSupport
   class TestCase
     fixtures :all
   end
