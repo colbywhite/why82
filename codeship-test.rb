f060168d732f4f94fdf1120e3a20e4df7beb6457
Colby M. White <colby.white@utexas.edu>
2015-09-30 23:50:32 -0500

    adding rspec to codeship #15

--- codeship-test.rb
+++ codeship-test.rb
@@ -2,5 +2,4 @@
 require 'rake'
 
 sh({ 'RAILS_ENV' => 'test' }, 'bundle exec rubocop')
-sh({ 'RAILS_ENV' => 'test' }, 'bundle exec rake db:reseed')
-sh({ 'RAILS_ENV' => 'test' }, 'bundle exec rake test')
+sh({ 'RAILS_ENV' => 'test' }, 'bundle exec rake spec')
