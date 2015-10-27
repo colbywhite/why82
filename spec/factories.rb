3b5e8e68ae8ab447e2729f5ed5fad3e8789df71a
Colby M. White <colby.white@utexas.edu>
2015-10-27 00:23:16 -0500

    correcting game module tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,23 @@
 FactoryGirl.define do
+  factory :league do
+    name 'Default League'
+    abbr 'DL'
+  end
+
   factory :season do
     name 'Default Season'
+    short_name '2016'
+    league
   end
 
   factory :team, aliases: [:home, :away] do
     name 'Default Test Team'
     abbr 'DTT'
-    season
   end
 
-  factory :game do
+  factory :nba2016_game do
     home
     away
     time { Time.zone.now }
-    season
   end
 end
--- spec/support/setup_helpers.rb
+++ spec/support/setup_helpers.rb
@@ -1,12 +1,14 @@
 module SetupHelpers
   # create 15 games, 10 of which involve the given team, 5 home and 5 away
   # TODO: need a more descriptive name for this method
-  def create_games_for_team(team)
+  def create_games_for_team(team, season = nil)
+    season = create(:season) unless season
+    game_sym = season.game_class.name.underscore.to_sym
     # create five random games that do not contain the team
-    games = (0..4).collect { create(:game) }
+    games = (0..4).collect { create(game_sym) }
     # create five home games for the team, then five away
-    games += (0..4).collect { create(:game, home: team) }
-    games += (0..4).collect { create(:game, away: team) }
+    games += (0..4).collect { create(game_sym, home: team) }
+    games += (0..4).collect { create(game_sym, away: team) }
     games
   end
 end
