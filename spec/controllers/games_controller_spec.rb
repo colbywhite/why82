5525c0a785af513590a524cfaa90a69f8a74e933
Colby M. White <colby.white@utexas.edu>
2015-12-02 18:33:56 -0600

    testing game grade endpoint

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe GamesController do
+  before :each do
+    @season = create(:season)
+    @game_table = season_to_game_sym @season
+  end
+
   describe 'GET #index' do
     def index(params = {})
       get :index, params
       assigns(:all)
-    end
-
-    before :each do
-      @season = create(:season)
-      @game_table = season_to_game_sym @season
     end
 
     describe 'paging' do
@@ -77,6 +77,63 @@ RSpec.describe GamesController do
         expect(returned_games[0]).to eq(@home_games[0])
         expect(returned_games[1]).to eq(@away_games[0])
       end
+    end
+  end
+
+  describe 'GET #graded' do
+    before(:each) do
+      #   create two 1s, two 2s, two 3s
+      @first_tier_1 = create(:team, name: 'First Tier 1')
+      give_team_record @game_table, @first_tier_1, 1, 1
+      @second_tier_1 = create(:team, name: 'Second Tier 1')
+      give_team_record @game_table, @second_tier_1, 10, TeamFilter::Record::TIER_ONE_CUTOFF
+
+      @first_tier_2 = create(:team, name: 'First Tier 2')
+      # halfway between tier two and tier one
+      halfway_percentage = TeamFilter::Record::TIER_TWO_CUTOFF +
+                           ((TeamFilter::Record::TIER_ONE_CUTOFF - TeamFilter::Record::TIER_TWO_CUTOFF) / 2)
+      give_team_record @game_table, @first_tier_2, 10, halfway_percentage
+      @second_tier_2 = create(:team, name: 'Second Tier 2')
+      give_team_record @game_table, @second_tier_2, 10, TeamFilter::Record::TIER_TWO_CUTOFF
+
+      @first_tier_3 = create(:team, name: 'First Tier 3')
+      # halfway between tier one and 0
+      halfway_percentage = TeamFilter::Record::TIER_ONE_CUTOFF / 2
+      give_team_record @game_table, @first_tier_3, 10, halfway_percentage
+      @second_tier_3 = create(:team, name: 'Second Tier 3')
+      # a third of the way between 0 and
+      third_percentage = TeamFilter::Record::TIER_ONE_CUTOFF / 3
+      give_team_record @game_table, @second_tier_3, 10, third_percentage
+
+      @game_date = Time.zone.local 2014, 12, 2, 19, 0, 0
+      create_round_robin_games @game_table, @game_date,
+                               @first_tier_1, @second_tier_1,
+                               @first_tier_2, @second_tier_2,
+                               @first_tier_3, @second_tier_3
+    end
+
+    it 'should grade games based on tiers' do
+      get :graded, start_date: @game_date.iso8601, season: @season.short_name
+      expect(response.status).to eq(200)
+      body = JSON.parse response.body
+      a_games = body['a']
+      b_games = body['b']
+      c_games = body['c']
+      d_games = body['d']
+      expect(a_games.size).to eq(1)
+      expect(a_games.first['home']['id']).to eq(@first_tier_1.id)
+      expect(a_games.first['away']['id']).to eq(@second_tier_1.id)
+
+      expect(b_games.size).to eq(4)
+      assert_all_games_contain_matching_team b_games, 'Tier 2'
+      assert_all_games_contain_matching_team b_games, 'Tier 1'
+
+      expect(c_games.size).to eq(1)
+      expect(c_games.first['home']['id']).to eq(@first_tier_2.id)
+      expect(c_games.first['away']['id']).to eq(@second_tier_2.id)
+
+      expect(d_games.size).to eq(9)
+      assert_all_games_contain_matching_team d_games, 'Tier 3'
     end
   end
 end
