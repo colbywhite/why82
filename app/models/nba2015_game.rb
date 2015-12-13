deed2637775ae760a6505308e8ac01d18a7f8a8b
Colby M. White <colby.white@utexas.edu>
2015-12-13 17:25:30 -0600

    add to_string to games for debugging

--- app/models/nba2015_game.rb
+++ app/models/nba2015_game.rb
@@ -1,5 +1,6 @@
 class Nba2015Game < ActiveRecord::Base
   extend Game
+  include Game
   include GameJsonSerialization
   belongs_to :home, class_name: 'Team', foreign_key: :home_id
   belongs_to :away, class_name: 'Team', foreign_key: :away_id
