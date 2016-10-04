f4947c8b8246870a3abc229e151416a7cb4a7788
Colby M. White <colbywhite@users.noreply.github.com>
2016-10-04 18:44:09 -0500

    using jade to build the HTML
    for multi reasons, most of them petty:
    - it's the 'cool' thing to do apparently
    - wait pug is the cool thing now? freaking js guys!!! :rage1:
    - i don't like writing html and marginally prefer anything else
    - the schedule.html and the tiers.html are very similar so i will
      eventually want some way to define a common layout. this seems like
      the most straightforward way. this is the only legit reason for this

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     entry: {
       schedule: "./js/schedule.jsx",
@@ -19,6 +22,9 @@ module.exports = {
               test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
               loader: "file"
             }, {
+              test: /\.json$/,
+              loader: "file"
+            }, {
               test: /\.(woff|woff2)$/,
               loader:"url?prefix=font/&limit=5000"
             }, {
@@ -26,7 +32,22 @@ module.exports = {
               loader: "url?limit=10000&mimetype=application/octet-stream"
             }, {
               test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-              loader: "url?limit=10000&mimetype=image/svg+xml" }
+              loader: "url?limit=10000&mimetype=image/svg+xml"
+            }, {
+              test: /\.jade$/,
+              loader: "jade",
+              query: {pretty: true}
+            }
         ]
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        chunks: ['schedule'],
+        template: 'schedule.jade',
+        filename: 'index.html'
+      }),
+      new CopyWebpackPlugin([
+        { from: 'data', to: 'data' }
+      ])
+    ]
 };
