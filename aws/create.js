e0b59cb40a38e921625e01ef5bcf402449d9f681
Colby M. White <colbywhite@users.noreply.github.com>
2017-09-16 19:01:50 -0500

    add ssl to cloudfront

--- aws/create.js
+++ aws/create.js
@@ -2,14 +2,23 @@ const AWSCFMonitor = require('aws-cf-monitor')
 const req = require('require-yml')
 const template = req('./stack')
 
+if (!template) {
+  throw new Error('template is undefined')
+}
+
 // use the same params that the AWS.CloudFormation object normally takes
 const site_name = 'why82'
+const cert_id = '1941f4c3-d4c7-4fad-9c44-d9e38196930f'
 const params = {
   StackName: `${site_name}-frontend-prod`,
   Parameters: [
     {
       ParameterKey: 'SiteName',
       ParameterValue: site_name,
+      UsePreviousValue: false
+    }, {
+      ParameterKey: 'ACMCertId',
+      ParameterValue: cert_id,
       UsePreviousValue: false
     }
   ],
